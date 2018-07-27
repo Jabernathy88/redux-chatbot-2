358b78614395a81d285c5b86f9a17781880fac05
Brocier <jschoe04@gmail.com>
2018-07-27 10:56:20 -0400

    Adding styles from react-dialogfow

--- src/App.js
+++ src/App.js
@@ -1,36 +1,109 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { sendMessage } from './Chat';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {sendMessage} from './Chat';
 import './styles.css'
 
 class App extends Component {
   render() {
-    const { feed, sendMessage } = this.props;
+    const {feed, sendMessage} = this.props;
+    const serverFeed = feed.map((entry, i) => {
+
+      switch (entry.sender) {
+        case "server":
+          return (
+            <div className="server-response float-right" key={i}>
+              {entry.text}
+              {console.log(entry)}
+            </div>
+          );
+          break;
+        case "user":
+          return (
+            <div className="user-request float-left" key={i}>
+              {entry.text}
+              {console.log(entry)}
+            </div>
+          );
+          break;
+        default:
+          break;
+      }
+    })
 
     return (
-      <div className="container">
-        <div className="container p-2 pt-4 bg-dark text-white">
-          <h2>Dialogflow Redux Text</h2>
-        </div>
-        <div className="bg-secondary">
-          <small>POWERED BY HELIUM</small>
-        </div>
-        <div className="container p-2">
-          <ul>
-            {feed.map((entry, i) => 
-              <li key={i}>
-                {entry.text}
-              </li>
-            )}
-          </ul>
-          <input type="text" 
-            onKeyDown={(event) => event.keyCode === 13 ? sendMessage(event.target.value) : null } />
+      <div id="content" className="">
+        {/* temp header */}
+        <header>
+          <div className="w-100">
+            <img className="header-img-logo" src="./images/helium-logo.png" alt=""/>
+          </div>
+          <img className="header-text-logo" src="./images/helium-text-logo.png" alt=""/>
+          <div className="blue-bar py-2 text-center w-100">
+            <h4>( Dialogflow Redux Mockup )</h4>
+          </div>
+        </header>
+
+        {/* <Main /> */}
+        <div className="container-fluid pt-2">
+          {/* <p><strong>Meet our new intern. Say hi!</strong></p>       */}
+
+          <div className="chat-container bg-white">
+            <div className="chat-header">
+              <div className="chat-header-wrapper w-100 d-flex align-items-center text-white">
+                <div className="chat-icon text-center d-inline-block d-inline-block m-4">
+                  <img className="chat-img-logo" src="./images/helium-white-icon.png" alt=""/>
+                </div>
+
+                <div className="d-inline-block">
+                  <div className="chat-title">
+                    <h4 className="font-weight-normal p-0">Katie</h4>
+                  </div>
+                  <div className="chat-subtitle font-weight-light">
+                    Helium Services
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="chat-top-nav text-right p-2 small">
+              <span className="text-white-50">POWERED BY
+              </span>
+              <strong className="text-light">HELIUM</strong>
+            </div>
+
+            <div className="chat-result w-100 pt-4">
+              {/* main content */}
+              <table className="chat-table">
+                <tbody>
+                  <tr>
+                    <td id="result" className="">
+                      <div>
+                        {serverFeed}
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="chat-input p-1">
+              <div id="chatForm" className="h-100 w-100" action="">
+                <input
+                  id="query"
+                  className="h-100 w-100"
+                  type="text"
+                  onKeyDown={(event) => event.keyCode === 13
+                  ? sendMessage(event.target.value)
+                  : null}/>
+              </div>
+              <div className="mic-icon"></div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({ feed: state });
+const mapStateToProps = (state) => ({feed: state});
 
-export default connect(mapStateToProps, { sendMessage })(App);
+export default connect(mapStateToProps, {sendMessage})(App);
--- src/Chat.js
+++ src/Chat.js
@@ -1,38 +1,47 @@
-import { ApiAiClient } from 'api-ai-javascript';
-import { applyMiddleware, createStore } from 'redux';
+import {ApiAiClient} from 'api-ai-javascript';
+import {applyMiddleware, createStore} from 'redux';
 
 const accessToken = '6085f9ad0c3b400cb36dcf32521f2c0c';
-const client = new ApiAiClient({ accessToken });
+const client = new ApiAiClient({accessToken});
 
 const ON_MESSAGE = 'ON_MESSAGE';
 
-export const sendMessage = (text, sender='user') => ({
+export const sendMessage = (text, sender = 'user') => ({
   type: ON_MESSAGE,
-  payload: { text, sender }
+  payload: {
+    text,
+    sender
+  }
 });
 
 const messageMiddleware = () => next => action => {
   next(action);
 
-  if(action.type === ON_MESSAGE ) {
-    const { text } = action.payload;
+  if (action.type === ON_MESSAGE) {
+    const {text} = action.payload;
 
-    client.textRequest(text)
-      .then( onSuccess )
+    client
+      .textRequest(text)
+      .then(onSuccess)
 
     function onSuccess(response) {
-      const {result: { fulfillment }} = response;
-      next(sendMessage( fulfillment.speech, 'server'));
+      const {result: {
+          fulfillment
+        }} = response;
+      next(sendMessage(fulfillment.speech, 'server'));
     }
   }
 };
 
-const initState = [{ text: 'Welcome!' }];
+const initState = [];
 
 const messageReducer = (state = initState, action) => {
   switch (action.type) {
     case ON_MESSAGE:
-      return [ ...state, action.payload ];
+      return [
+        ...state,
+        action.payload
+      ];
     default:
       return state;
   }
