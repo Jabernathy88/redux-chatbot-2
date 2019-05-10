31c0866d8e2cc2d1b4df97c7debe7e366eaa7fef
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-05-10 09:00:54 -0400

    Remove old elements.

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ export default class App extends Component {
       <div id="content" className="">
         <header>
           <div className="w-100">
-            <img className="header-img-logo" src="./images/helium-logo.png" alt=""/>
+            <br />
+            <br />
           </div>
-          <img className="header-text-logo" src="./images/helium-text-logo.png" alt=""/>
           <div className="blue-bar py-2 text-center w-100">
-            <h4>( Dialogflow Redux Mockup )</h4>
+            <h4>Dialogflow React-Redux Mockup</h4>
           </div>
         </header>
-
+        <br />
         {this.state.haveUserInfo
           ? <ChatBot/>
           : <UserInfoForm toggleHaveUserInfo={this.toggleHaveUserInfo}/>}
